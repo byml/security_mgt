4490361a219f9f8c1d40ef1c31513fc1793a1713
zhiyong.liu@optilinktech.com <zhiyong.liu@optilinktech.com>
2016-07-26 16:56:19 +0800

    改为不复权行情价

--- app/models/security_price_history.rb
+++ app/models/security_price_history.rb
@@ -3,13 +3,13 @@ require 'rest-client'
 class SecurityPriceHistory < ActiveRecord::Base
   belongs_to :security_info
 
-  URL_SH_AND_SZ = 'http://123.57.249.182:8080/URL/Api/ApiInterface.ashx?apikey=3960f9e2bdf438e80d04e7637d2e6f7a&flag=gethData&'
+  URL_SH_AND_SZ = 'http://123.57.249.182:8080/URL/Api/ApiInterface.ashx?apikey=3960f9e2bdf438e80d04e7637d2e6f7a&flag=histData'
 
   def self.record_history(security_info, response)
     if response.code == 200
       result = JSON.parse(response.body)
       if result['rows'].size > 0
-        security_price_history = SecurityPriceHistory.new(result['rows'][0].except('code'))
+        security_price_history = SecurityPriceHistory.new(result['rows'][0].slice('trade_date','open','close','high','low','volume'))
         security_price_history.security_info = security_info
         security_price_history.save
       end
@@ -35,6 +35,8 @@ class SecurityPriceHistory < ActiveRecord::Base
   end
 
   def self.query_and_record_histories_in_range(security_infos, from_date, to_date)
+    from_date = Date.parse(from_date) if 'String' == from_date.class.name
+    to_date = Date.parse(to_date) if 'String' == to_date.class.name
     (from_date..to_date).each do |trade_date|
       query_and_record_histories(security_infos, trade_date)
     end
